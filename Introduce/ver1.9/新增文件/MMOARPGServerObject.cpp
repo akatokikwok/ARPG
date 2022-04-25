b68e0b2e3b61832a5f41d94fbd94745ff1bf7495
DESKTOP-6OVVB6Q\Askyr1m <akatokikwok@outlook.com>
2022-04-25 18:04:31 +0800

    [feat, ver1.09]: 续写MysqlConfig,架构dbServer这个引擎独立项目

---
+++ Introduce/ver1.9/新增文件/MMOARPGServerObject.cpp
@@ -0,0 +1,25 @@
+#include "MMOARPGServerObject.h"
+
+void UMMOARPGServerObejct::Init()
+{
+	Super::Init();
+
+}
+
+void UMMOARPGServerObejct::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void UMMOARPGServerObejct::Close()
+{
+	Super::Close();
+
+}
+
+void UMMOARPGServerObejct::RecvProtocol(uint32 InProtocol)
+{
+	Super::RecvProtocol(InProtocol);
+
+}
