009a16ecde89089b2afd970ad33ed30c67b38095
DESKTOP-6OVVB6Q\Askyr1m <akatokikwok@outlook.com>
2022-05-16 16:31:16 +0800

    [feat]: 串联CreatePanel和RenamePanel之间的数据流通

--- Source/MMOARPG/UI/Hall/Element/UI_CharacterCreatePanel.cpp
+++ Source/MMOARPG/UI/Hall/Element/UI_CharacterCreatePanel.cpp
@@ -15,8 +15,30 @@ void UUI_CharacterCreatePanel::NativeDestruct()
 	Super::NativeDestruct();
 }
 
+/**  负责 当点击加号后创建 捏脸信息的逻辑.*/
+void UUI_CharacterCreatePanel::CreateKneadFace()
+{
+	// 1.先把滑动块里所有元素清掉.
+	List->ClearChildren();
+	// 2.创建并弹出1个捏脸控件并注册进滑动框.
+	if (UI_KneadFaceClass != nullptr) {
+		if (UUI_KneadFace* InUIKnead = CreateWidget<UUI_KneadFace>(GetWorld(), UI_KneadFaceClass)) {
+			if (UScrollBoxSlot* InScrollSlot = Cast<UScrollBoxSlot>(List->AddChild(InUIKnead))) {
+				InScrollSlot->SetPadding(10.0f);// 设置被添加进的元素的间距为10.0f;
+			}
+		}
+	}
+}
+
+void UUI_CharacterCreatePanel::CreateCharacterButtons()
+{
+	InitCharacterButton(4.0f);
+}
+
 void UUI_CharacterCreatePanel::InitCharacterButton(const int32 InNumber)
 {
+	List->ClearChildren();// 先清空所有元素.
+
 	if (UI_CharacterButtonClass != nullptr) {
 		for (int32 i = 0; i < InNumber; ++i) {
 			if (UUI_CharacterButton* InCharButton = CreateWidget<UUI_CharacterButton>(GetWorld(), UI_CharacterButtonClass)) {
---
+++ Source/MMOARPG/UI/Hall/Element/UI_KneadFace.h
@@ -0,0 +1,31 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "../../../UI/Core/UI_Base.h"
+#include "Components/CheckBox.h"
+#include "Components/WidgetSwitcher.h"
+#include "UI_KneadFace.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MMOARPG_API UUI_KneadFace : public UUI_Base
+{
+	GENERATED_BODY()
+	
+private:
+// 	UPROPERTY(meta = (BindWidget))
+// 		UCheckBox* Body;
+// 
+// 	UPROPERTY(meta = (BindWidget))
+// 		UCheckBox* Appearance;
+// 
+// 	UPROPERTY(meta = (BindWidget))
+// 		UCheckBox* Talent;
+// 
+// 	UPROPERTY(meta = (BindWidget))
+// 		UWidgetSwitcher* FacePanel;
+};
