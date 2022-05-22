9c06954b30a98c2aa0f338dd5ba61fc833db6cab
DESKTOP-6OVVB6Q\Askyr1m <akatokikwok@outlook.com>
2022-05-22 16:02:33 +0800

    [feat]:Move the viewport vertically for Stage Character.

---
+++ Plugins/SimpleActorBrowsing/Source/SimpleActorBrowsing/Public/Core/SimplePanelMove.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Tickable.h"
+
+namespace SimpleActorAction
+{
+	class SIMPLEACTORBROWSING_API FSimplePanelMove : public FTickableGameObject
+	{
+	public:
+		FSimplePanelMove();
+
+		//Monitored by main thread 
+		virtual void Tick(float DeltaTime);
+		virtual TStatId GetStatId() const;
+
+		void Register(APlayerController* InController, AActor* InTarget, float InDistance);
+
+		void BeginMove();
+		void EndMove();
+
+		void ResetTarget(AActor* InNewTarget);
+	protected:
+		bool bMove;// 启用视口上下移动功能.
+		APlayerController* Controller;
+		TWeakObjectPtr<AActor> Target;
+		FVector2D MousePos_last;
+		FVector Location;// 垂直长度上的中点.
+		float Distance;// 垂直总长度; 但计算时候一般取一半.
+	};
+}
