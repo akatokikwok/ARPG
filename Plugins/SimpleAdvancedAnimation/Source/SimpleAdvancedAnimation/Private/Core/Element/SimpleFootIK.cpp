bad3fbdaaf150f57f99fe8aecf6a186e5dbca13a
DESKTOP-6OVVB6Q\Askyr1m <akatokikwok@outlook.com>
2022-06-02 03:52:12 +0800

    [feat]: Write the architecture designs a singleton FSimpleAdvancedAnimSystem to manage IK.
    架构设计出一个单例FSimpleAdvancedAnimSystem管理IK.

---
+++ Plugins/SimpleAdvancedAnimation/Source/SimpleAdvancedAnimation/Private/Core/Element/SimpleFootIK.cpp
@@ -0,0 +1,96 @@
+﻿#include "Core/Element/SimpleFootIK.h"
+#include "Components/SkeletalMeshComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "GameFramework/Character.h"
+#include "Kismet/KismetSystemLibrary.h"
+
+FSimpleFootIK::FSimpleFootIK()
+	: Character(NULL)
+	, TraceDistance(50.f)
+	, InterpSpeed(18.f)
+	, bPendingKill(false)
+{
+
+}
+
+void FSimpleFootIK::Tick(float DeltaTime)
+{
+	for (auto& Tmp :  IKInfos) {
+		// 本只脚的理想偏移.
+		float OffsetTarget = this->FootTrace(Tmp.Key, TraceDistance);
+
+		// 本只脚当前偏移 往 理想偏移 执行渐变插值.
+		Tmp.Value.Offset = FMath::FInterpTo(Tmp.Value.Offset, OffsetTarget, DeltaTime, InterpSpeed);
+	}
+}
+
+void FSimpleFootIK::Init(ACharacter* InCharacter, const TArray<FName>& InBoneNames, float InTraceDistance /*= 50.f*/, float InInterpSpeed /*= 18.f*/, float InTraceStart /*= 50.f*/)
+{
+	Character = InCharacter;
+	TraceDistance = InTraceDistance;
+	InterpSpeed = InInterpSpeed;
+// 	TraceStart = InTraceStart;
+	for (auto& BoneName_single : InBoneNames) {
+		IKInfos.Add(BoneName_single, FFootIKInfo());
+	}
+}
+
+float FSimpleFootIK::FindOffset(const FName& InKeyName)
+{
+	if (FFootIKInfo* Info = FindFootIKInfo(InKeyName)) {
+		return Info->Offset;
+	}
+
+	return 0.f;
+}
+
+FFootIKInfo* FSimpleFootIK::FindFootIKInfo(const FName& InKeyName)
+{
+	return IKInfos.Find(InKeyName);
+}
+
+float FSimpleFootIK::FootTrace(const FName& BoneName, float InTraceDistance)
+{
+	if (Character != nullptr) {
+		if (Character->IsPendingKill()) {// 人被销毁了.
+ 			bPendingKill = true;
+			Character = nullptr;
+			return 0.0f;
+		}
+
+		// 射线起点.
+		FVector BoneLoc = Character->GetMesh()->GetSocketLocation(BoneName);
+		// 射线终点.
+		float CharacterLocZ = Character->GetActorLocation().Z;
+		float HalfHeight = Character->GetCapsuleComponent()->GetScaledCapsuleHalfHeight();
+
+		FVector StartLoc = BoneLoc;
+		FVector EndLoc = FVector(StartLoc.X, StartLoc.Y,
+			CharacterLocZ - (HalfHeight + InTraceDistance)
+		);
+
+		// 执行LineTrace.
+		FHitResult HitResult;
+		TArray<AActor*> Ignores;
+		if (UKismetSystemLibrary::LineTraceSingle(
+			Character->GetWorld(),
+			StartLoc,
+			EndLoc,
+			ETraceTypeQuery::TraceTypeQuery1,
+			false,
+			Ignores,
+			EDrawDebugTrace::Type::ForOneFrame,
+			HitResult,
+			true))
+		{
+			/**
+			 * HitResult.Location 是从上往下打的射线打到地面或者硬质地板的交合点.
+			 * HitResult.TraceEnd 是忽略地板阻挡本来预计要打到的点(即人为预设的检测终点.)
+			 * (HitResult.Location - HitResult.TraceEnd).Size() - InTraceDistance 求得真正的 脚位移到地板上的那段offset.
+			 */
+			return (HitResult.Location - HitResult.TraceEnd).Size() - InTraceDistance;// 脚相对于地板的偏移,这是个负值.
+		}
+	}
+
+	return 0.0f;// 检测失败返回0
+}
---
+++ Plugins/SimpleAdvancedAnimation/Source/SimpleAdvancedAnimation/Private/Core/SimpleAdvancedAnimSystem.cpp
@@ -0,0 +1,80 @@
+﻿#include "Core/SimpleAdvancedAnimSystem.h"
+FSimpleAdvancedAnimSystem* FSimpleAdvancedAnimSystem::AdvancedAnimSystem = nullptr;
+
+FSimpleAdvancedAnimSystem* FSimpleAdvancedAnimSystem::Get()
+{
+	if (!AdvancedAnimSystem) {
+		AdvancedAnimSystem = new FSimpleAdvancedAnimSystem();
+	}
+
+	return AdvancedAnimSystem;
+}
+
+void FSimpleAdvancedAnimSystem::Destroy()
+{
+	if (AdvancedAnimSystem) {
+		delete AdvancedAnimSystem;
+		AdvancedAnimSystem = nullptr;
+	}
+}
+
+TStatId FSimpleAdvancedAnimSystem::GetStatId() const
+{
+	return TStatId();
+}
+
+FSAAHandle FSimpleAdvancedAnimSystem::CreateFootIK(ACharacter* InCharacter, const TArray<FName>& InBoneNames, float InTraceDistance /*= 50.f*/, float InInterpSpeed /*= 18.f*/, float InTraceStart /*= 50.f*/)
+{
+	FSAAHandle Handle = FMath::RandRange(0, 999999);
+	if (!FootIKs.Contains(Handle)) {
+		// 构造1个并填值.
+		FSimpleFootIK& InFootIK = FootIKs.Add(Handle, FSimpleFootIK());
+		InFootIK.Init(InCharacter, InBoneNames, InTraceDistance, InInterpSpeed, InTraceStart);
+
+		return Handle;
+	}
+	// 循环递归创数据直至查询表内不再有"重复"元素.
+	return CreateFootIK(InCharacter, InBoneNames, InTraceDistance, InInterpSpeed);
+}
+
+FSimpleFootIK* FSimpleAdvancedAnimSystem::FindFootIK(const FSAAHandle InKey)
+{
+	return FootIKs.Find(InKey);
+}
+
+float FSimpleAdvancedAnimSystem::FindOffset(const FSAAHandle InKey, const FName& InKeyName)
+{
+	if (FFootIKInfo* InInfo = FindFootIKInfo(InKey, InKeyName)) {
+		return InInfo->Offset;
+	}
+
+	return 0.0f;
+}
+
+FFootIKInfo* FSimpleAdvancedAnimSystem::FindFootIKInfo(const FSAAHandle InKey, const FName& InKeyName)
+{
+	if (FSimpleFootIK* InFootIK = FindFootIK(InKey)) {
+		return InFootIK->FindFootIKInfo(InKeyName);
+	}
+
+	return nullptr;
+}
+
+void FSimpleAdvancedAnimSystem::Tick(float DeltaTime)
+{
+	// Tick 表内所有FootIK.
+ 	for (auto& FootIK_per : FootIKs) {
+ 		FootIK_per.Value.Tick(DeltaTime);
+ 	}
+
+ 	// 从FootIK查询表内移除那些 人物已经失效了的行.
+ 	TArray<FSAAHandle> RemoveHandle;
+ 	for (auto& FootIk_Itr : FootIKs) {
+ 		if (FootIk_Itr.Value.IsPendingKill()) {
+ 			RemoveHandle.Add(FootIk_Itr.Key);
+ 		}
+ 	}
+ 	for (auto& Tmp : RemoveHandle) {
+ 		FootIKs.Remove(Tmp);
+ 	}
+}
