9e11979b57438a5d64fb5056279aa461a6ac5c3b
DESKTOP-6OVVB6Q\Askyr1m <akatokikwok@outlook.com>
2022-06-04 13:18:19 +0800

    [feat]: Make the visual effect of switching UI after the specified character frame is selected.
    制作选定指定人物框后切换UI的视觉效果.

--- Source/MMOARPG/UI/Game/Partner/UI_PartnerInfo.h
+++ Source/MMOARPG/UI/Game/Partner/UI_PartnerInfo.h
@@ -17,6 +17,8 @@ UCLASS()
 class MMOARPG_API UUI_PartnerInfo : public UUI_Base
 {
 	GENERATED_BODY()
+private:
+	friend class UUI_PartnerList;
 
 	UPROPERTY(meta = (BindWidget))
 	UCheckBox* ClickedCheckBox;
@@ -34,12 +36,20 @@ class MMOARPG_API UUI_PartnerInfo : public UUI_Base
 	UProgressBar* ManaBar;
 
 public:
+	UUI_PartnerInfo(const FObjectInitializer& ObjectInitializer);
 	virtual void NativeConstruct();
-
 	virtual void NativeDestruct();
-
-public:
-
+	// 回调; 当点了CheckBox.
 	UFUNCTION()
 	void OnClickedCharacter(bool bClicked);
+	// 拿人物信息控件哪个实例的ID号.
+	int32 GetCharacterID() { return CharacterID; }
+	// 设定本CheckBox是否处于选中外观.
+	void ShowSelected(bool bShow);
+protected:
+	// 设置人物信息控件的ID号.
+	void SetCharacterID(int32 InCharacterID) { CharacterID = InCharacterID; }
+	
+protected:
+	int32 CharacterID;// 第几号人物.
 };
--- Source/MMOARPG/UI/Game/Partner/UI_PartnerList.h
+++ Source/MMOARPG/UI/Game/Partner/UI_PartnerList.h
@@ -23,8 +23,11 @@ class MMOARPG_API UUI_PartnerList : public UUI_Base
 	UPROPERTY(EditDefaultsOnly, Category = UI)
 	TSubclassOf<UUI_PartnerInfo> UI_PartnerInfoClass;
 public:
-
 	virtual void NativeConstruct();
-
 	virtual void NativeDestruct();
+	// 初始化外观.
+	void Init();
+	// 反复遍历列表里的 人物信息框 , 直至 指定的仿函数操作该任务框失败.
+	void CallCharacterListByPredicate(TFunction<bool(UUI_PartnerInfo* Info)> InFun);
+
 };
