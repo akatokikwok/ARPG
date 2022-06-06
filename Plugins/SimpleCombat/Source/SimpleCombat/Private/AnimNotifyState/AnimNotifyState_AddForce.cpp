6845eb79ef30b59769b1cb80f6085a58d620052e
DESKTOP-6OVVB6Q\Askyr1m <akatokikwok@outlook.com>
2022-06-07 01:21:45 +0800

    [feat]: Create an AnimNotifyState dedicated to applying decay force in the plugin, then Tick decay it frame by frame, and add NotifyState_AddForce in the blueprint seq.
    在插件里制造一个专门用于施加衰减力的AnimNotifyState,然后Tick逐帧衰减它,并在蓝图seq里添加NotifyState_AddForce.

---
+++ Plugins/SimpleCombat/Source/SimpleCombat/Private/AnimNotifyState/AnimNotifyState_AddForce.cpp
@@ -0,0 +1,68 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AnimNotifyState/AnimNotifyState_AddForce.h"
+#include "GameFramework/Character.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+UAnimNotifyState_AddForce::UAnimNotifyState_AddForce()
+	: Super()
+	, DirectionForce(1.f, 0.f, 0.f)
+	, ForceSize(1800000.f)
+	, TotalDurationConsuming(0.f)
+	, ForceSizeConsuming(0.f)
+{
+
+}
+
+void UAnimNotifyState_AddForce::NotifyBegin(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float TotalDuration)
+{
+	Super::NotifyBegin(MeshComp, Animation, TotalDuration);
+	TotalDurationConsuming = TotalDuration;// 记录一下总时长.
+	ForceSizeConsuming = ForceSize;// 记录/拷贝一份总力大小.
+}
+
+void UAnimNotifyState_AddForce::NotifyTick(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float FrameDeltaTime)
+{
+	Super::NotifyTick(MeshComp, Animation, FrameDeltaTime);
+	if (TotalDurationConsuming > 0.0f) {
+		if (ACharacter* InCharacter = Cast<ACharacter>(MeshComp->GetOuter())) {
+			// 给人施加推力.
+			FVector NewDirection = CalCurrentCharacterDirection(InCharacter);
+			InCharacter->GetCharacterMovement()->AddForce(ForceSizeConsuming * NewDirection);
+
+			if (ForceSizeConsuming > 0.0f) {
+				// 每秒帧数
+				float PreSecondFrame = 1.f / FrameDeltaTime;
+				// 这段时间的总消耗帧数
+				float TotalFrame = TotalDurationConsuming * PreSecondFrame;
+				// 单帧分摊到的力.
+				float ForceAttenuationFrameDeltaTime = ForceSize / TotalFrame;
+				// 从总力里逐帧扣除单帧力.
+				ForceSizeConsuming -= ForceAttenuationFrameDeltaTime;
+			}
+		}
+	}
+}
+
+void UAnimNotifyState_AddForce::NotifyEnd(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation)
+{
+	Super::NotifyEnd(MeshComp, Animation);
+
+}
+
+FVector UAnimNotifyState_AddForce::CalCurrentCharacterDirection(ACharacter* InCharacter)
+{
+	if (InCharacter) {
+		if (DirectionForce.X != 0.f) {
+			return InCharacter->GetActorForwardVector() * DirectionForce.X;
+		}
+		else if (DirectionForce.Y != 0.f) {
+			return InCharacter->GetActorRightVector() * DirectionForce.Y;
+		}
+		else if (DirectionForce.Z != 0.f) {
+			return InCharacter->GetActorUpVector() * DirectionForce.Z;
+		}
+	}
+	return FVector::ZeroVector;
+}
