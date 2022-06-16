e923887b15380643ba398edab0e9e5c5702e0bb2
DESKTOP-NONF38J\Administrator <akatokikwok@outlook.com>
2022-06-16 19:02:08 +0800

    [part2]:

---
+++ Plugins/SimpleCombat/Source/SimpleCombat/Private/AnimNotifyState/AnimNotifyState_StopAnimMontage.cpp
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AnimNotifyState/AnimNotifyState_StopAnimMontage.h"
+#include "GameFramework/Character.h"
+#include "GameFramework/CharacterMovementComponent.h"
+void UAnimNotifyState_StopAnimMontage::NotifyBegin(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float TotalDuration)
+{
+	Super::NotifyBegin(MeshComp, Animation, TotalDuration);
+
+}
+
+void UAnimNotifyState_StopAnimMontage::NotifyTick(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float FrameDeltaTime)
+{
+	Super::NotifyTick(MeshComp, Animation, FrameDeltaTime);
+	if (ACharacter* InCharacter = Cast<ACharacter>(MeshComp->GetOuter())) {
+		if (InCharacter->GetCharacterMovement()->Velocity.Size() >= 5.f) {
+			InCharacter->StopAnimMontage();
+		}
+	}
+}
+
+void UAnimNotifyState_StopAnimMontage::NotifyEnd(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation)
+{
+	Super::NotifyEnd(MeshComp, Animation);
+
+}
