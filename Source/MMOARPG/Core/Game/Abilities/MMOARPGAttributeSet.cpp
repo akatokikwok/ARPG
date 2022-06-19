feb58849e2ab087350d0053ae9e7d4fdaa3090a6
DESKTOP-6OVVB6Q\Askyr1m <akatokikwok@outlook.com>
2022-06-19 10:22:24 +0800

    [feat]: Improved attribute synchronization for MMOARPGAttributeSet.
    完善MMOARPGAttributeSet的属性同步.

--- Source/MMOARPG/Core/Game/Abilities/MMOARPGAttributeSet.cpp
+++ Source/MMOARPG/Core/Game/Abilities/MMOARPGAttributeSet.cpp
@@ -1,6 +1,10 @@
 ﻿#include "MMOARPGAttributeSet.h"
 
 UMMOARPGAttributeSet::UMMOARPGAttributeSet()
+	: Health(100.f)
+	, MaxHealth(100.f)
+	, Mana(0.f)
+	, MaxMana(0.f)
 {
 
 }
@@ -9,6 +13,41 @@ UMMOARPGAttributeSet::UMMOARPGAttributeSet()
 void UMMOARPGAttributeSet::PostGameplayEffectExecute(const FGameplayEffectModCallbackData& Data)
 {
 	Super::PostGameplayEffectExecute(Data);
+
+}
+
+// 覆写 同步变量需要重写的方法.
+void UMMOARPGAttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME(UMMOARPGAttributeSet, Health);
+	DOREPLIFETIME(UMMOARPGAttributeSet, MaxHealth);
+	DOREPLIFETIME(UMMOARPGAttributeSet, Mana);
+	DOREPLIFETIME(UMMOARPGAttributeSet, MaxMana);
+}
+
+void UMMOARPGAttributeSet::OnRep_Health(const FGameplayAttributeData& OldValue)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UMMOARPGAttributeSet, Health, OldValue);
+
+}
+
+void UMMOARPGAttributeSet::OnRep_MaxHealth(const FGameplayAttributeData& OldValue)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UMMOARPGAttributeSet, MaxHealth, OldValue);
+
+}
+
+void UMMOARPGAttributeSet::OnRep_Mana(const FGameplayAttributeData& OldValue)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UMMOARPGAttributeSet, Mana, OldValue);
+
+}
+
+void UMMOARPGAttributeSet::OnRep_MaxMana(const FGameplayAttributeData& OldValue)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UMMOARPGAttributeSet, MaxMana, OldValue);
 
 }
 
