f8f453eb2d45c692fc2de73360799094c3ff02fb
DESKTOP-6OVVB6Q\Askyr1m <akatokikwok@outlook.com>
2022-06-24 01:33:53 +0800

    [feat]: Added Notify_FaceTarget specifically to force the caster to face the hit.
    新增Notify_FaceTarget专门处理迫使施法者朝向受击者.

---
+++ Plugins/SimpleCombat/Source/SimpleCombat/Public/AnimNotify/AnimNotify_FaceTarget.h
@@ -0,0 +1,33 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotify.h"
+#include "AnimNotify_FaceTarget.generated.h"
+
+/**
+ * Notify: 让施法者朝向面向敌人目标
+ */
+UCLASS(meta = (DisplayName = "FaceTarget"))
+class SIMPLECOMBAT_API UAnimNotify_FaceTarget : public UAnimNotify
+{
+	GENERATED_BODY()
+
+public:
+	UAnimNotify_FaceTarget();
+
+	virtual FString GetNotifyName_Implementation() const override;
+	virtual void Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation) override;
+
+public:
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Face Target")
+		bool bYaw;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Face Target")
+		bool bPitch;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Face Target")
+		bool bRoll;
+};
