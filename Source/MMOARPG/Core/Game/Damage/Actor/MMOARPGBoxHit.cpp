6f7dfb622eb2ef274846518290e7a7fe15912e80
DESKTOP-6OVVB6Q\Askyr1m <akatokikwok@outlook.com>
2022-07-01 01:12:06 +0800

    [feat]: Temporarily disables PVP and only enables human boxhit damage for mushroom monsters.
    暂时关闭PVP,仅开启人的boxhit伤害仅对蘑菇怪物生效.

--- Source/MMOARPG/Core/Game/Damage/Actor/MMOARPGBoxHit.cpp
+++ Source/MMOARPG/Core/Game/Damage/Actor/MMOARPGBoxHit.cpp
@@ -33,43 +33,47 @@ void AMMOARPGBoxHit::HandleDamage(
 	Super::HandleDamage(OverlappedComponent, OtherActor, OtherComp, OtherBodyIndex, bFromSweep, SweepResult);
 
 	if (GetInstigator() != OtherActor) {// 首先不能打到自己.
-		if (AMMOARPGCharacterBase* InTarget = Cast<AMMOARPGCharacterBase>(OtherActor)) {// 只承认只接收与人形态的生物hitbox关联.
+		if (AMMOARPGCharacterBase* InPawnSelf = Cast<AMMOARPGCharacterBase>(GetInstigator())) {/* 施法者得存在.*/
 
-			if (!InTarget->IsDie()) {/* 防止敌人死亡后还能被用户攻击.*/
+			if (AMMOARPGCharacterBase* InTarget = Cast<AMMOARPGCharacterBase>(OtherActor)) {// 只承认只接收与人形态的生物hitbox关联.
+				if (InTarget->GetCharacterType() != InPawnSelf->GetCharacterType() || 0) {/* 防止同类人打同类人. 0代表卡PVP开关. */
 
-				/* 组1个游玩事件参数. */
-				FGameplayEventData EventData;
-				EventData.Instigator = GetInstigator();// 施法者
-				EventData.Target = OtherActor;// 攻击目标,打到谁了
+					if (!InTarget->IsDie()) {/* 防止敌人死亡后还能被用户攻击.*/
+						/* 组1个游玩事件参数. */
+						FGameplayEventData EventData;
+						EventData.Instigator = GetInstigator();// 施法者
+						EventData.Target = OtherActor;// 攻击目标,打到谁了
 
-				// 处理任意一个buff, 给所有与刀刃接触到的有效敌人对象（带有GameplayAbilityComponent）添加GATag.
-				for (auto& Tmp : AHitCollision::Buffs) {
-					/** 接收到伤害时候,会把伤害信息用此API把伤害事件与Tag传出去,传至GA::基类里的AbilityTask_PMAWDamage里 */
-					/** 在数据上 专门处理受击, 击伤 */
-					UAbilitySystemBlueprintLibrary::SendGameplayEventToActor(
-						GetInstigator(),
-						/*FGameplayTag::RequestGameplayTag(TEXT("Player.Attack.ComboLinkage")),*/
-						FGameplayTag::RequestGameplayTag(Tmp),
-						EventData
-					);
+						// 处理任意一个buff, 给所有与刀刃接触到的有效敌人对象（带有GameplayAbilityComponent）添加GATag.
+						for (auto& Tmp : AHitCollision::Buffs) {
+							/** 接收到伤害时候,会把伤害信息用此API把伤害事件与Tag传出去,传至GA::基类里的AbilityTask_PMAWDamage里 */
+							/** 在数据上 专门处理受击, 击伤 */
+							UAbilitySystemBlueprintLibrary::SendGameplayEventToActor(
+								GetInstigator(),
+								/*FGameplayTag::RequestGameplayTag(TEXT("Player.Attack.ComboLinkage")),*/
+								FGameplayTag::RequestGameplayTag(Tmp),
+								EventData
+							);
+						}
+
+						// 查询死亡, 单一执行死亡或者受击挨打.
+						if (InTarget->IsDie()) {
+							InTarget->PlayDie();
+						}
+						else {
+							// 让挨打的人面朝向 施法者.
+							FRotator TargetRot = (-EventData.Instigator->GetActorForwardVector()).ToOrientationRotator();
+							InTarget->SetActorRotation(TargetRot);
+
+							// 先去注册人物内部受击ID
+							InTarget->SetHitID(AHitCollision::GetHitID());
+							// 再执行人物挨打受击.
+							InTarget->PlayHit();
+						}
+
+						/* 处理受击的逻辑. */
+					}
 				}
-				
-				// 查询死亡, 单一执行死亡或者受击挨打.
-				if (InTarget->IsDie()) {
-					InTarget->PlayDie();
-				}
-				else {
-					// 让挨打的人面朝向 施法者.
-					FRotator TargetRot = (-EventData.Instigator->GetActorForwardVector()).ToOrientationRotator();
-					InTarget->SetActorRotation(TargetRot);
-
-					// 先去注册人物内部受击ID
-					InTarget->SetHitID(AHitCollision::GetHitID());
-					// 再执行人物挨打受击.
-					InTarget->PlayHit();
-				}
-
-				/* 处理受击的逻辑. */
 			}
 		}
 	}
