5e07cd2766509c9bd6ca0b81a76cbdec6a0126d8
DESKTOP-6OVVB6Q\Askyr1m <akatokikwok@outlook.com>
2022-08-26 22:51:03 +0800

    [feat]: 下载的时候补充逻辑:对下载进度,资源名称,资源块大小分别执行打印.

--- Source/MMOARPG/UI/HotUpdate/UI_HotUpdateMain.cpp
+++ Source/MMOARPG/UI/HotUpdate/UI_HotUpdateMain.cpp
@@ -2,6 +2,8 @@
 #include "Object/SimpleHotUpdateObject.h"
 #include "ThreadManage.h"
 #include "Kismet/GameplayStatics.h"
+#include "Components/ProgressBar.h"
+#include "Components/TextBlock.h"
 
 #define LOCTEXT_NAMESPACE "UUI_HotUpdateMain"
 
@@ -32,7 +34,20 @@ void UUI_HotUpdateMain::NativeDestruct()
 
 void UUI_HotUpdateMain::Downloading(float InValue, const FString& InName, int32 TotalBytes, int32 BytesReceived)
 {
-
+	if (DownloadProgress) {
+		// 打印下载进度
+		DownloadProgress->SetPercent(InValue);
+	}
+	if (DownloadedResourceName) {
+		// 打印资源名字
+		DownloadedResourceName->SetText(FText::FromString(InName));
+	}
+	if (DownloadedDataSize) {
+		// 打印资源块大小
+		DownloadedDataSize->SetText(
+			FText::Format(LOCTEXT("Downloading_DownloadDataSize", "{0}/{1}"), FText::AsMemory(TotalBytes), FText::AsMemory(BytesReceived)
+		));
+	}
 }
 
 void UUI_HotUpdateMain::CheckVersion(EServerVersionResponseType InType)
