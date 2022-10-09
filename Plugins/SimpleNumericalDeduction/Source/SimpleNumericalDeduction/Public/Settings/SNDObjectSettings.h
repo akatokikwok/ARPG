a05aff3d0754c837d47eeb67a076c9a4cc1f7ce9
DESKTOP-6OVVB6Q\Askyr1m <akatokikwok@outlook.com>
2022-10-09 21:27:01 +0800

    [feat]: Settings文件负责管理推演的基础表,次数, 推演属性数据这一系列字段.

---
+++ Plugins/SimpleNumericalDeduction/Source/SimpleNumericalDeduction/Public/Settings/SNDObjectSettings.h
@@ -0,0 +1,34 @@
+﻿#pragma once
+#include "CoreMinimal.h"
+#include "SimpleNumericalDeductionType.h"
+#include "SNDObjectSettings.generated.h"
+class UDataTable;
+class UNumericalAlgorithmExecuteObject;
+
+/**
+ *
+ */
+UCLASS(config = SNDObjectSettings)
+class USNDObjectSettings : public UObject
+{
+	GENERATED_BODY()
+public:
+	USNDObjectSettings();
+
+public:
+	// 推演的基础表
+	UPROPERTY(EditAnywhere, Category = "DeductionSettings", meta = (ToolTip = "This content is version base, which is used for automatic iteration."))
+		UDataTable* BaseTable;
+
+	// 推演的次数
+	UPROPERTY(config, EditAnywhere, Category = "DeductionSettings", meta = (ToolTip = "This content is version base, which is used for automatic iteration."))
+		int32 DeductionNumber;
+
+	// 被生成的具体属性数据; UPROPERTY(config)表示直接生成的不可被编辑.
+	UPROPERTY(config, VisibleAnywhere, Category = "InvisibleData", meta = (ToolTip = "This content is version base, which is used for automatic iteration."))
+		TArray<FDeduceAttributeDataTables> AttributeDatas;
+
+// 	// 算法模板; UPROPERTY(config)表示直接生成的不可被编辑.
+// 	UPROPERTY(config, EditAnywhere, Category = "AlgorithmExecuteObject", meta = (ToolTip = "This content is version base, which is used for automatic iteration."))
+// 		TArray<TSubclassOf<UNumericalAlgorithmExecuteObject>> NumericalAlgorithmExecuteObjects;
+};
