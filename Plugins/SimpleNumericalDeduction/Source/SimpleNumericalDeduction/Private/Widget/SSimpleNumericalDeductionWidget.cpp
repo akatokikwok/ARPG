ea67ecfdc9a84b914737fcb5a79f2009e19dbda4
DESKTOP-6OVVB6Q\Askyr1m <akatokikwok@outlook.com>
2022-10-09 21:53:22 +0800

    [feat]: SSimpleNumericalDeductionWidget推演控件基础框架渲染搭建.

--- Plugins/SimpleNumericalDeduction/Source/SimpleNumericalDeduction/Private/Widget/SSimpleNumericalDeductionWidget.cpp
+++ Plugins/SimpleNumericalDeduction/Source/SimpleNumericalDeduction/Private/Widget/SSimpleNumericalDeductionWidget.cpp
@@ -1,10 +1,67 @@
 ﻿#include "Widget/SSimpleNumericalDeductionWidget.h"
-
+#include "Widgets/Layout/SScrollBox.h"
+#include "Widgets/Layout/SBox.h"
+#include "PropertyEditorModule.h"
+#include "Settings/SNDObjectSettings.h"
+#include "Widget/Slate/SDataTableAttributeTable.h"
+#include "UObject/NumericalAlgorithmExecuteObject.h"
+#include "Misc/FileHelper.h"
 #define LOCTEXT_NAMESPACE "SimpleNumericalDeductionWidget"// 定义本地化操作
 
 void SSimpleNumericalDeductionWidget::Construct(const FArguments& InArgs)
 {
-	
+	ChildSlot
+	[
+		SNew(SScrollBox)
+		.Orientation(Orient_Vertical)
+		+ SScrollBox::Slot()
+		[
+			SNew(SVerticalBox)
+			+ SVerticalBox::Slot().AutoHeight()
+			[
+				/* 垂直框里塞1个水平框.*/
+				SNew(SHorizontalBox)
+				// 先在靠右侧放1个按钮
+				+ SHorizontalBox::Slot().HAlign(EHorizontalAlignment::HAlign_Right).Padding(4.f, 2.f, 4.f, 2.f)
+				[
+					SNew(SButton)
+					.Text(LOCTEXT("SND_Save_as_Default", "Save as Default"))
+					.HAlign(HAlign_Center)
+					.OnClicked(this, &SSimpleNumericalDeductionWidget::SaveAsDefault)
+					.ToolTipText(LOCTEXT("SND_Save_as_DefaultTip", "Store configuration locally."))
+				]
+				// 靠右侧再放1个按钮, 它管理CSV
+				+ SHorizontalBox::Slot().HAlign(EHorizontalAlignment::HAlign_Right).Padding(4.f, 2.f, 4.f, 2.f).AutoWidth()
+				[
+					SNew(SButton)
+					.Text(LOCTEXT("SND_Save_as_csv", "Save as csv"))
+					.HAlign(HAlign_Center)
+					.IsEnabled(this, &SSimpleNumericalDeductionWidget::IsEnableToCSV)
+					.OnClicked(this, &SSimpleNumericalDeductionWidget::SaveAsCSV)
+					.ToolTipText(LOCTEXT("SND_Save_as_CSVTip", "This function is mainly used for calling gameplay system after exporting deduction results."))
+				]
+
+			]
+		]
+	];
+}
+
+FReply SSimpleNumericalDeductionWidget::SaveAsDefault()
+{
+
+	return FReply::Handled();
+}
+
+bool SSimpleNumericalDeductionWidget::IsEnableToCSV() const
+{
+
+	return false;
+}
+
+FReply SSimpleNumericalDeductionWidget::SaveAsCSV()
+{
+
+	return FReply::Handled();
 }
 
 #undef LOCTEXT_NAMESPACE// 终止定义本地化操作
--- Plugins/SimpleNumericalDeduction/Source/SimpleNumericalDeduction/Public/Widget/SSimpleNumericalDeductionWidget.h
+++ Plugins/SimpleNumericalDeduction/Source/SimpleNumericalDeduction/Public/Widget/SSimpleNumericalDeductionWidget.h
@@ -12,9 +12,16 @@ public:
 	{}
 	SLATE_END_ARGS()
 public:
+	/** Slate控件的渲染入口都从Construct开始. */
 	void Construct(const FArguments& InArgs);
 
 public:
+	// 接口: 储存为默认
+	FReply SaveAsDefault();
+	// 接口: 检验是否满足CSV条件
+	bool IsEnableToCSV()const;
+	// 接口: 储存为CSV
+	FReply SaveAsCSV();
 
 private:
 	TSharedPtr<class SVerticalBox> VerticalList;
