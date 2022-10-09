a13c6bac03debaeae52ce9ec4faf15933cf14c84
DESKTOP-6OVVB6Q\Askyr1m <akatokikwok@outlook.com>
2022-10-09 21:26:15 +0800

    [feat]: 定义1个关联的推演算法对象.

--- Plugins/SimpleNumericalDeduction/Source/SimpleNumericalDeduction/Public/UObject/NumericalAlgorithmExecuteObject.h
+++ Plugins/SimpleNumericalDeduction/Source/SimpleNumericalDeduction/Public/UObject/NumericalAlgorithmExecuteObject.h
@@ -1,3 +1,15 @@
-#pragma once
+﻿#pragma once
 #include "CoreMinimal.h"
 #include "SimpleNumericalDeductionType.h"
+#include "NumericalAlgorithmExecuteObject.generated.h"
+
+/**
+ * 数值推演算法的对象
+ */
+UCLASS(BlueprintType, Blueprintable)
+class SIMPLENUMERICALDEDUCTION_API UNumericalAlgorithmExecuteObject : public UObject
+{
+	GENERATED_BODY()
+public:
+	virtual float GetAlgorithmValue() { return 0.f; }
+};
