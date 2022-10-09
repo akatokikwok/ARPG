32bf165a1017f66213777cf3325c173e7defea2c
DESKTOP-6OVVB6Q\Askyr1m <akatokikwok@outlook.com>
2022-10-09 21:25:32 +0800

    [feat]: 在Type.h这张文件里 新增推演属性元数据及其承载的表.

--- Plugins/SimpleNumericalDeduction/Source/SimpleNumericalDeduction/Public/SimpleNumericalDeductionType.h
+++ Plugins/SimpleNumericalDeduction/Source/SimpleNumericalDeduction/Public/SimpleNumericalDeductionType.h
@@ -1,3 +1,41 @@
 ﻿#pragma once
-
 #include "CoreMinimal.h"
+#include "SimpleNumericalDeductionType.generated.h"
+class UNumericalAlgorithmExecuteObject;
+
+/**
+ * 推演属性元数据
+ */
+USTRUCT(BlueprintType)
+struct FDeduceAttributeData
+{
+	GENERATED_USTRUCT_BODY()
+public:
+
+public:
+	// 代表LV 蓝量 血量 攻击诸如此类的Key字段.
+	UPROPERTY()
+		FName Key;
+	// 可配置的系数.
+	UPROPERTY()
+		float Coefficient;
+	// 推演算法.
+	UPROPERTY()
+		TSubclassOf<UNumericalAlgorithmExecuteObject> BaseAlgorithm;
+};
+
+/**
+ * 承载 "推演属性元数据"的表
+ */
+USTRUCT(BlueprintType)
+struct FDeduceAttributeDataTables
+{
+	GENERATED_USTRUCT_BODY()
+public:
+	// 推演属性 表名.
+	UPROPERTY()
+		FName TableName;
+	// 多个推演元数据.
+	UPROPERTY()
+		TArray<FDeduceAttributeData> AttributeDatas;
+};
