16bee18c4c15c478432fc300d53527c4df510121
DESKTOP-6OVVB6Q\Askyr1m <akatokikwok@outlook.com>
2022-10-11 22:32:47 +0800

    [feat]: SSDataTableAttributeTable扩展框头部和body编写.

---
+++ Plugins/SimpleNumericalDeduction/Source/SimpleNumericalDeduction/Private/Widget/Slate/SDataTableAttributeTable.cpp
@@ -0,0 +1,52 @@
+﻿#include "Widget/Slate/SDataTableAttributeTable.h"
+#include "Widgets/Layout/SExpandableArea.h"
+#include "Settings/SNDObjectSettings.h"
+#include "SimpleNumericalDeduction.h"
+
+#define LOCTEXT_NAMESPACE "SSDataTableAttributeTable"
+
+void SSDataTableAttributeTable::Construct(const FArguments& InArgs, FDeduceAttributeDataTables& InDeduceAttributeDataTables)
+{
+	AttributeDataTablesPtr = &InDeduceAttributeDataTables;// 缓存入参的 推演表
+
+	ChildSlot
+	[
+		/* 塞入一个扩展框*/
+		SNew(SExpandableArea)
+		
+		.HeaderContent()// 定义扩展框的"头部"
+		[
+			SNew(SVerticalBox)
+			+ SVerticalBox::Slot()
+			[
+				SNew(SButton)
+				.HAlign(HAlign_Center)
+				.Text(this, &SSDataTableAttributeTable::GetTableName)
+				.OnClicked(this, &SSDataTableAttributeTable::SpawnNewAttributeCurveTable)
+				.ToolTipText(LOCTEXT("TableName_Tip", "Changes in the curve can be generated."))
+			]
+		]
+
+		.BodyContent()// 定义扩展框的"body"
+		[
+			SAssignNew(ListVerticalBox, SVerticalBox)
+		]
+	];
+}
+
+FReply SSDataTableAttributeTable::SpawnNewAttributeCurveTable()
+{
+	
+	return FReply::Handled();
+}
+
+FText SSDataTableAttributeTable::GetTableName() const
+{
+	if (AttributeDataTablesPtr) {
+		return FText::FromName(AttributeDataTablesPtr->TableName);
+	}
+	// 没有属性的话就给一个默认的本地化值
+	return LOCTEXT("AttributeDataTables_TableName", "NONE_Attribute");
+}
+
+#undef LOCTEXT_NAMESPACE 
---
+++ Plugins/SimpleNumericalDeduction/Source/SimpleNumericalDeduction/Public/Widget/Slate/SDataTableAttributeTable.h
@@ -0,0 +1,30 @@
+﻿#pragma once
+#include "CoreMinimal.h"
+#include "SimpleNumericalDeductionType.h"
+#include "Widgets/SCompoundWidget.h"
+class SVerticalBox;
+
+/**
+ * 玩家基础属性编辑器控件, 派生自SCompoundWidget.
+ */
+class SSDataTableAttributeTable :public SCompoundWidget
+{
+public:
+	SLATE_BEGIN_ARGS(SSDataTableAttributeTable)
+	{}
+	SLATE_END_ARGS()
+public:
+	/** Slate控件的渲染入口都从Construct开始. */
+	void Construct(const FArguments& InArgs, FDeduceAttributeDataTables& InDeduceAttributeDataTables);
+public:
+	/** 生成1个新的曲线表 */
+	FReply SpawnNewAttributeCurveTable();
+	/** 拿取表名给button用 */
+	FText GetTableName() const;
+
+private:
+	//
+	TSharedPtr<SVerticalBox> ListVerticalBox;
+	// 承载 "推演属性元数据"的表
+	FDeduceAttributeDataTables* AttributeDataTablesPtr;
+};
