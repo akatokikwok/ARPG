0b27a2b1c1a11d5f84194d9dc3e60b5d26560d2a
DESKTOP-NONF38J\Administrator <akatokikwok@outlook.com>
2022-11-04 17:39:26 +0800

    [feat]:新增关联CurveTreeItem的结构体,维护构造一个TreeWiget并创建曲线的Model

--- Plugins/SimpleNumericalDeduction/Source/SimpleNumericalDeduction/Private/Widget/Editor/DeduceAttributeCurveTable.cpp
+++ Plugins/SimpleNumericalDeduction/Source/SimpleNumericalDeduction/Private/Widget/Editor/DeduceAttributeCurveTable.cpp
@@ -22,8 +22,93 @@ struct FSimpleEditorDACTable
 	static const FName DeduceAttributeCurveTableID;
 };
 const FName FSimpleEditorDACTable::DeduceAttributeCurveTableID = TEXT("SimpleEditorDACTable");
+//////////////////////////////////////////////////////////////////////////
 
+/**
+ * 各列(Label, Select, Pin)
+ */
+struct FSimpleEditorColumnNames
+{
+	static const FName Label;
+	static const FName Select;
+	static const FName Pin;
+};
+const FName FSimpleEditorColumnNames::Label = TEXT("Label");
+const FName FSimpleEditorColumnNames::Select = TEXT("Select");
+const FName FSimpleEditorColumnNames::Pin = TEXT("Pin");
+//////////////////////////////////////////////////////////////////////////
 
+#pragma region 用于曲线编辑器的TreeItem结构体
+/**
+ * 用于曲线编辑器的TreeItem
+ */
+struct FDACAssetEditorTreeItem : public ICurveEditorTreeItem
+{
+	// 带参构造器
+	FDACAssetEditorTreeItem(TWeakObjectPtr<UCurveBase> InCurveOwner, const FRichCurveEditInfo& InEditInfo);
+
+public:
+	// 覆写 构建treewidget的虚方法
+	virtual TSharedPtr<SWidget> GenerateCurveEditorTreeWidget(const FName& InColumnName, TWeakPtr<FCurveEditor> InCurveEditor, FCurveEditorTreeItemID InTreeItemID, const TSharedRef<ITableRow>& TableRow) override;
+	// 覆写 创建曲线的model
+	virtual void CreateCurveModels(TArray<TUniquePtr<FCurveModel>>& OutCurveModels) override;
+private:
+	TWeakObjectPtr<UCurveBase> CurveOwner;// 具体的曲线
+	FRichCurveEditInfo EditInfo;// 富文本内容
+	FText CurveName;// 曲线名称
+	FLinearColor CurveColor;// 曲线颜色
+};
+
+/** 带参构造器 */
+FDACAssetEditorTreeItem::FDACAssetEditorTreeItem(TWeakObjectPtr<UCurveBase> InCurveOwner, const FRichCurveEditInfo& InEditInfo)
+	: CurveOwner(InCurveOwner)
+	, EditInfo(InEditInfo)
+{
+	if (CurveOwner.IsValid()) {
+		CurveName = FText::FromName(EditInfo.CurveName);// 曲线名字由富文本内容初始化
+		CurveColor = FLinearColor(FMath::RandRange(0.f, 1.f), FMath::RandRange(0.f, 1.f), FMath::RandRange(0.f, 1.f), 1.f);// 曲线颜色初始化
+		//CurveOwner->GetCurveColor(EditInfo);
+	}
+}
+
+/** 覆写 构建treewidget的虚方法 */
+TSharedPtr<SWidget> FDACAssetEditorTreeItem::GenerateCurveEditorTreeWidget(const FName& InColumnName, TWeakPtr<FCurveEditor> InCurveEditor, FCurveEditorTreeItemID InTreeItemID, const TSharedRef<ITableRow>& TableRow)
+{
+	// 按列名称种类划分各自的构造逻辑.
+	if (InColumnName == FSimpleEditorColumnNames::Label) {
+		return SNew(SHorizontalBox)
+			+ SHorizontalBox::Slot().Padding(FMargin(4.f)).VAlign(VAlign_Center).HAlign(HAlign_Right).AutoWidth()
+			[
+				SNew(STextBlock).Text(CurveName).ColorAndOpacity(FSlateColor(CurveColor))
+			];
+	}
+	else if (InColumnName == FSimpleEditorColumnNames::Select) {
+		return SNew(SCurveEditorTreeSelect, InCurveEditor, InTreeItemID, TableRow);
+	}
+	else if (InColumnName == FSimpleEditorColumnNames::Pin) {
+		return SNew(SCurveEditorTreePin, InCurveEditor, InTreeItemID, TableRow);
+	}
+
+	return nullptr;
+}
+
+/**  覆写 创建曲线的model */
+void FDACAssetEditorTreeItem::CreateCurveModels(TArray<TUniquePtr<FCurveModel>>& OutCurveModels)
+{
+	if (!CurveOwner.IsValid()) {
+		return;
+	}
+	// 先创建并定义1个富文本Model的uniqueptr
+	TUniquePtr<FRichCurveEditorModelRaw> NewCurve = MakeUnique<FRichCurveEditorModelRaw>(static_cast<FRichCurve*>(EditInfo.CurveToEdit), CurveOwner.Get());
+	NewCurve->SetShortDisplayName(CurveName);
+	NewCurve->SetColor(CurveColor);
+	// CurveModel被存到数组里
+	OutCurveModels.Add(MoveTemp(NewCurve));
+}
+//////////////////////////////////////////////////////////////////////////
+#pragma endregion 用于曲线编辑器的TreeItem结构体
+
+#pragma region 曲线Table编辑器
 FSDeduceAttributeCurveTable::FSDeduceAttributeCurveTable()
 	: DeduceAttributeDataTables(nullptr)
 	, DeduceAttributeDataTable(nullptr)
@@ -107,5 +192,6 @@ TSharedRef<SDockTab> FSDeduceAttributeCurveTable::SpawnTab_CurveAsset(const FSpa
 
 	return NewDockTab;
 }
+#pragma endregion 曲线Table编辑器
 
 #undef LOCTEXT_NAMESPACE
