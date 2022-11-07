490142513142c03383b5591111b3dbc82337848f
DESKTOP-6OVVB6Q\Askyr1m <akatokikwok@outlook.com>
2022-11-07 22:45:38 +0800

    [feat]: Widget/Editor下架设1个"负责调试属性"的元元素控件.

--- Plugins/SimpleNumericalDeduction/Source/SimpleNumericalDeduction/Private/Widget/Editor/DebugAttributeDeduce.cpp
+++ Plugins/SimpleNumericalDeduction/Source/SimpleNumericalDeduction/Private/Widget/Editor/DebugAttributeDeduce.cpp
@@ -1,1 +1,29 @@
-﻿
+﻿#include "Widget/Editor/DebugAttributeDeduce.h"
+#include "Log/SimpleNumericalDeductionLog.h"
+
+
+#define LOCTEXT_NAMESPACE "FDebugAttibuteDeduce"
+
+FDebugAttibuteDeduce::FDebugAttibuteDeduce()
+{
+
+}
+
+void FDebugAttibuteDeduce::InitLayout()
+{
+
+}
+
+void FDebugAttibuteDeduce::Construct()
+{
+
+}
+
+TSharedRef<SDockTab> FDebugAttibuteDeduce::SpawnTab_DebugSettingWidget(const FSpawnTabArgs& Args)
+{
+	TSharedRef<SDockTab> NewDockTab = SNew(SDockTab);
+
+	return NewDockTab;
+}
+
+#undef LOCTEXT_NAMESPACE
--- Plugins/SimpleNumericalDeduction/Source/SimpleNumericalDeduction/Public/Widget/Editor/DebugAttributeDeduce.h
+++ Plugins/SimpleNumericalDeduction/Source/SimpleNumericalDeduction/Public/Widget/Editor/DebugAttributeDeduce.h
@@ -1,1 +1,21 @@
-﻿
+﻿#pragma once
+#include "CoreMinimal.h"
+#include "SimpleNumericalDeductionType.h"
+
+/**
+ * "负责调试属性"的元元素控件
+ */
+class FDebugAttibuteDeduce
+{
+public:
+	FDebugAttibuteDeduce();
+	void InitLayout();
+	void Construct();
+
+public:
+	/** 生成调试用的 标签页 */
+	TSharedRef<SDockTab> SpawnTab_DebugSettingWidget(const FSpawnTabArgs& Args);
+
+private:
+	TSharedPtr<SWidget> WidgetBase;
+};
