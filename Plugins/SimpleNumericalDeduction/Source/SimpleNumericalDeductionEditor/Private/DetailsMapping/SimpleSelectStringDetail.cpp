c466eb4e8fe6b4f1487211d7951ace48d53be4b7
DESKTOP-6OVVB6Q\Askyr1m <akatokikwok@outlook.com>
2022-11-13 18:26:09 +0800

    [Squash, feat, phase3]: 在CustomizeHeader这边新增一段关于入参变量的句柄推导过程.
    [chore]: 增加下拉菜单SelectStrings的注入时机以及GET_MEMBER_NAME_CHECKED的过程. (+1 squashed commits)

--- Plugins/SimpleNumericalDeduction/Source/SimpleNumericalDeductionEditor/Private/DetailsMapping/SimpleSelectStringDetail.cpp
+++ Plugins/SimpleNumericalDeduction/Source/SimpleNumericalDeductionEditor/Private/DetailsMapping/SimpleSelectStringDetail.cpp
@@ -15,17 +15,39 @@ void FSimpleSelectStringDetail::CustomizeHeader(TSharedRef<IPropertyHandle> Prop
 {
 	// I. 制作1个builder; 严格意义上此builder即为一堆属性的载体窗口
 	FMenuBuilder CharacterAttributeModeBuilder(true, nullptr);
-	for (size_t i = 0; i < 10; ++i) {
-		FString HelloStr = TEXT("Hello");
-		FUIAction CharacterAction(
-			FExecuteAction::CreateSP(this, &FSimpleSelectStringDetail::HandleCharacterKey, HelloStr)
-		);
 
-		CharacterAttributeModeBuilder.AddMenuEntry(LOCTEXT("Hello", "Hello"),
-			LOCTEXT("HelloTip", "HelloTip"),
-			FSlateIcon(),
-			CharacterAction
-		);
+	/** 先检测下拉菜单变量的handle; 下拉菜单里关联的SelectStrings是主角、配角、蘑菇怪的表名 */
+	if (TSharedPtr<IPropertyHandle> StringsArrayPropertyHandle = 
+			PropertyHandle->GetChildHandle(GET_MEMBER_NAME_CHECKED(FSimpleSelectString, SelectStrings))) {
+		// 1.转成Array形态的句柄
+		if (TSharedPtr<IPropertyHandleArray> ArrayHandle = StringsArrayPropertyHandle->AsArray()) {
+			// 入参字段的提取数组;
+			TArray<FString> MyStrings;
+			
+			// 2. 扫描 提取出的Array Handle里的所有元素并注入 MyStrings
+			uint32 Number = 0;
+			ArrayHandle->GetNumElements(Number);
+			for (uint32 i = 0; i < Number; i++) {
+				if (TSharedPtr<IPropertyHandle> StringElementHandle = ArrayHandle->GetElement(i)) {
+					FString& Ele = MyStrings.AddDefaulted_GetRef();
+					StringElementHandle->GetValueAsFormattedString(Ele);
+				}
+			}
+
+			// 3. 扫描入参字段内句柄元素, 制作该builder
+			for (size_t i = 0; i < MyStrings.Num(); ++i) {
+				FUIAction CharacterAction(
+					FExecuteAction::CreateSP(this, &FSimpleSelectStringDetail::HandleCharacterKey, MyStrings[i])
+				);
+
+				CharacterAttributeModeBuilder.AddMenuEntry(
+					FText::FromString(MyStrings[i]),
+					LOCTEXT("HelloTip", "HelloTip"),
+					FSlateIcon(),
+					CharacterAction
+				);
+			}
+		}
 	}
 
 	// II. 把Builder构建在HeadRow内
--- Plugins/SimpleNumericalDeduction/Source/SimpleNumericalDeductionRuntime/Private/SimpleNumericalDeductionType.cpp
+++ Plugins/SimpleNumericalDeduction/Source/SimpleNumericalDeductionRuntime/Private/SimpleNumericalDeductionType.cpp
@@ -12,7 +12,7 @@ FSimpleSelectString::FSimpleSelectString()
 	if (USNDObjectSettings* SND = const_cast<USNDObjectSettings*>(GetDefault<USNDObjectSettings>())) {
 		for (auto& Tmp : SND->AttributeDatas) {
 			// 把SNDObj的总数据存入下拉菜单里
-			SelectStrings.Add(Tmp.TableName.ToString());
+			SelectStrings.Add(Tmp.TableName.ToString());// 表名(主角, 仆从, 蘑菇怪)
 		}
 	}
 }
