616d53bc0a2fc846826498218475f78de4ee25d5
DESKTOP-6OVVB6Q\Askyr1m <akatokikwok@outlook.com>
2022-12-05 23:33:06 +0800

    [pref, p0]: 技能SkillTable里放弃原有的分来源设计,改用统一的技能类.

--- Source/MMOARPG/DataTable/CharacterSkillTable.h
+++ Source/MMOARPG/DataTable/CharacterSkillTable.h
@@ -18,25 +18,41 @@ struct FCharacterSkillTable :public FTableRowBase
 	GENERATED_USTRUCT_BODY()
 public:
 	FCharacterSkillTable();
-public:
-	TSubclassOf<UGameplayAbility>* FindComboAttack(const FName& InKey);// 用Key获取缓存池:combo形式
-	TSubclassOf<UGameplayAbility>* FindSkillAttack(const FName& InKey);// 用Key获取缓存池:skill形式
-	TSubclassOf<UGameplayAbility>* FindLimbs(const FName& InKey);// 用Key获取缓存池:肢体行为
+// public:
+// 	TSubclassOf<UGameplayAbility>* FindComboAttack(const FName& InKey);// 用Key获取缓存池:combo形式
+// 	TSubclassOf<UGameplayAbility>* FindSkillAttack(const FName& InKey);// 用Key获取缓存池:skill形式
+// 	TSubclassOf<UGameplayAbility>* FindLimbs(const FName& InKey);// 用Key获取缓存池:肢体行为
 
 public:
 	// ID号, 存在于负责技能的DTRow.
 	UPROPERTY(EditDefaultsOnly, Category = "CharacterSkill")
-		int32 ID;
+		int32 ID;// ID号, 存在于负责技能的DTRow.
 	
-	// 普攻(连招)形式的攻击 缓存池, 存在于负责技能的DTRow;
+	// 技能图标
 	UPROPERTY(EditDefaultsOnly, Category = "CharacterSkill")
-		TArray<TSubclassOf<UGameplayAbility>> ComboAttack;// 攻击缓存池:连招形式, 存在于负责技能的DTRow;
+		UTexture2D* Icon;// 技能图标
 	
-	// 技能形式的攻击 缓存池, 存在于负责技能的DTRow;
+	// 技能冷却CD
 	UPROPERTY(EditDefaultsOnly, Category = "CharacterSkill")
-		TArray<TSubclassOf<UGameplayAbility>> SkillAttack;// 攻击缓存池:技能形式, 存在于负责技能的DTRow;
+		float CD;// 技能冷却CD
 
-	// 和肢体行为相关的一些能力; 例如受击, 死亡, 嘲讽 打招呼
-	UPROPERTY(EditDefaultsOnly, Category = "CharacterLimbs")
-		TArray<TSubclassOf<UGameplayAbility>> Limbs;// 肢体行为缓存池; 
+	// 技能属性提示
+	UPROPERTY(EditDefaultsOnly, Category = "CharacterSkill")
+		FText SkillAttributeTip;// 技能属性提示
+
+	// 具体技能类
+	UPROPERTY(EditDefaultsOnly, Category = "CharacterSkill")
+		TSubclassOf<UGameplayAbility> GameplayAbility;// 具体技能类
+
+// 	// 普攻(连招)形式的攻击 缓存池, 存在于负责技能的DTRow;
+// 	UPROPERTY(EditDefaultsOnly, Category = "CharacterSkill")
+// 		TArray<TSubclassOf<UGameplayAbility>> ComboAttack;// 攻击缓存池:连招形式, 存在于负责技能的DTRow;
+// 	
+// 	// 技能形式的攻击 缓存池, 存在于负责技能的DTRow;
+// 	UPROPERTY(EditDefaultsOnly, Category = "CharacterSkill")
+// 		TArray<TSubclassOf<UGameplayAbility>> SkillAttack;// 攻击缓存池:技能形式, 存在于负责技能的DTRow;
+// 
+// 	// 和肢体行为相关的一些能力; 例如受击, 死亡, 嘲讽 打招呼
+// 	UPROPERTY(EditDefaultsOnly, Category = "CharacterLimbs")
+// 		TArray<TSubclassOf<UGameplayAbility>> Limbs;// 肢体行为缓存池; 
 };
