653df94d99bf15e333841dc27755ec26da8b59e0
DESKTOP-NONF38J\Administrator <akatokikwok@outlook.com>
2023-01-29 11:43:44 +0800

    [feat, phase2]:逻辑重新封装,并增加刷新skillslot数据与外观的lambda.

--- Source/MMOARPG/UI/Game/Skill/UI_UnderSkillGroup.cpp
+++ Source/MMOARPG/UI/Game/Skill/UI_UnderSkillGroup.cpp
@@ -36,20 +36,36 @@ void UUI_UnderSkillGroup::NativeTick(const FGeometry& MyGeometry, float InDeltaT
 
 void UUI_UnderSkillGroup::LayoutSlot(const TArray<FName>& InSkillTags)
 {
-	if (SlotArray->GetChildrenCount() == 0) {// 表明是重新创建技能组
+	if (/*!SlotArray->GetChildrenCount() && */SkillSlotClass) {
+		if (AMMOARPGCharacterBase* InCharacterBase = GetWorld()->GetFirstPlayerController()->GetPawn<AMMOARPGCharacterBase>()) {
+			if (AMMOARPGGameState* InGameState = GetWorld()->GetGameState<AMMOARPGGameState>()) {
+				TArray<FCharacterSkillTable*> SkillTables;// 主角身上的所有技能
+				int32 CharacterID = InCharacterBase->GetID();
+				if (InGameState->GetCharacterSkillsTables(CharacterID, SkillTables)) {// 能提取出主角身上的所有技能
 
-		if (/*!SlotArray->GetChildrenCount() && */SkillSlotClass) {
-			if (AMMOARPGCharacterBase* InCharacterBase = GetWorld()->GetFirstPlayerController()->GetPawn<AMMOARPGCharacterBase>()) {
-				if (AMMOARPGGameState* InGameState = GetWorld()->GetGameState<AMMOARPGGameState>()) {
+					// Lambda-刷新某个skillslot所有数据和外观
+					auto UpdateSlotWidget = [&](int32 InMyRow, UUI_SkillSlot* InSkillSlotWidget) ->void {
+						if (InSkillTags.IsValidIndex(InMyRow)) {
+							// 检索所有 匹配的技能行
+							for (auto& SkillTmp : SkillTables) {
+								if (UMMOARPGGameplayAbility* InMMOGA = Cast<UMMOARPGGameplayAbility>(SkillTmp->GameplayAbility->GetDefaultObject())) {
+									if (InSkillTags[InMyRow].ToString() == InMMOGA->AbilityTags.ToStringSimple()) {// 找出名字一致的那一行
+										// 刷新这个skillslot
+										InSkillSlotWidget->Update(InSkillTags[InMyRow],
+											SkillTmp->Icon,
+											InMMOGA->CostValue("Mana", InCharacterBase->GetCharacterLevel()));
+										break;
+									}
+								}
+							}
+						}
+					};
 
-					TArray<FCharacterSkillTable*> SkillTables;
-					int32 CharacterID = InCharacterBase->GetID();
-					if (InGameState->GetCharacterSkillsTables(CharacterID, SkillTables)) {
-
+					if (SlotArray->GetChildrenCount() == 0) {/// 表明是重新创建技能组
 						int32 RowNumber = 5;
 						for (int32 MyRow = 0; MyRow < RowNumber; MyRow++) {
 							if (UUI_SkillSlot* SlotWidget = CreateWidget<UUI_SkillSlot>(GetWorld(), SkillSlotClass)) {
-								//
+								// 横框设定为skillslot的父级面板
 								SlotWidget->SetParents(this);
 
 								//
@@ -59,69 +75,50 @@ void UUI_UnderSkillGroup::LayoutSlot(const TArray<FName>& InSkillTags)
 									BoxSlot->SetVerticalAlignment(EVerticalAlignment::VAlign_Fill);
 								}
 
-								// 更新具体数据和技能
-								if (InSkillTags.IsValidIndex(MyRow)) {
-									// 检索所有 匹配的技能行
-									for (auto& SkillTmp : SkillTables) {
-										if (UMMOARPGGameplayAbility* InMMOGA = Cast<UMMOARPGGameplayAbility>(SkillTmp->GameplayAbility->GetDefaultObject())) {
-											if (InSkillTags[MyRow].ToString() == InMMOGA->AbilityTags.ToStringSimple()) {// 找出名字一致的那一行
-												// 刷新这个skillslot
-												SlotWidget->Update(InSkillTags[MyRow], 
-													SkillTmp->Icon, 
-													InMMOGA->CostValue("Mana", InCharacterBase->GetCharacterLevel()));
-												break;
-											}
-										}
+								// 刷新某个skillslot所有数据和外观
+								UpdateSlotWidget(MyRow, SlotWidget);
+							}
+						}
+					}
+					else {/// 表明是刷新或重新更新技能组
+						int32 Index = 0;
+						CallSKillSlot([&](UUI_SkillSlot* InSkillSlot) ->bool {
+							if (InSkillSlot) {
+								if (InSkillTags.IsValidIndex(Index)) {
+									/* 不相等则表明 客户端模拟的不正确,需由服务器去矫正 */
+									if (InSkillSlot->GetSlotInfo().Tags != InSkillTags[Index]) {
+										// 先清除原先的图标
+										InSkillSlot->ResetIcon();
+										InSkillSlot->GetSlotInfo().Reset();
+
+										// 刷新某个skillslot所有数据和外观
+										UpdateSlotWidget(Index, InSkillSlot);
+
+									#pragma region Deprached
+										//// 先查找到和入参匹配的技能行
+										//if (FCharacterSkillTable** MySkillTableRow = SkillTables.FindByPredicate(
+										//	[&](FCharacterSkillTable* InTableRowInfo)->bool {
+										//		if (UGameplayAbility* InGA = Cast<UGameplayAbility>(InTableRowInfo->GameplayAbility->GetDefaultObject())) {
+										//			if (InSkillTags[Index].ToString() == InGA->AbilityTags.ToStringSimple()) {
+										//				return true;
+										//			}
+										//		}
+										//		return false;
+										//	})) {
+										//	// 这个技能槽去 更新 上一步符合条件的技能行里的图标
+										//	InSkillSlot->Update(InSkillTags[Index], (*MySkillTableRow)->Icon);
+										//}
+									#pragma endregion Deprached
 									}
 								}
 							}
-						}
+							Index++;// 计数迭代
+							return false;
+							});
 					}
 				}
 			}
 		}
-	}
-	else {// 表明是刷新或重新更新技能组
-
-		int32 Index = 0;
-		CallSKillSlot([&](UUI_SkillSlot* InSkillSlot) ->bool {
-			if (InSkillSlot) {
-				if (InSkillTags.IsValidIndex(Index)) {
-					/* 不相等则表明 客户端模拟的不正确,需由服务器去矫正 */
-					if (InSkillSlot->GetSlotInfo().Tags != InSkillTags[Index]) {
-						if (AMMOARPGCharacterBase* InCharacterBase = GetWorld()->GetFirstPlayerController()->GetPawn<AMMOARPGCharacterBase>()) {
-							if (AMMOARPGGameState* InGameState = GetWorld()->GetGameState<AMMOARPGGameState>()) {
-								TArray<FCharacterSkillTable*> SkillTables;
-								int32 CharacterID = InCharacterBase->GetID();
-								if (InGameState->GetCharacterSkillsTables(CharacterID, SkillTables)) {
-									// 先清除原先的图标
-									InSkillSlot->ResetIcon();
-									InSkillSlot->GetSlotInfo().Reset();
-
-									// 先查找到和入参匹配的技能行
-									if (FCharacterSkillTable** MySkillTableRow = SkillTables.FindByPredicate(
-										[&](FCharacterSkillTable* InTableRowInfo)->bool {
-											if (UGameplayAbility* InGA = Cast<UGameplayAbility>(InTableRowInfo->GameplayAbility->GetDefaultObject())) {
-												if (InSkillTags[Index].ToString() == InGA->AbilityTags.ToStringSimple()) {
-													return true;
-												}
-											}
-											return false;
-										})) {
-										// 这个技能槽去 更新 上一步符合条件的技能行里的图标
-										InSkillSlot->Update(InSkillTags[Index], (*MySkillTableRow)->Icon);
-									}
-								}
-							}
-						}
-					}
-					//
-				}
-			}
-			Index++;// 计数迭代
-
-			return false;
-			});
 	}
 }
 
