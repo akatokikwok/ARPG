cd106e729eb53c72fc71d32687429e49d6e691e0
DESKTOP-6OVVB6Q\Askyr1m <akatokikwok@outlook.com>
2023-02-04 20:30:04 +0800

    [feat, phase4]:被挑飞至空中的敌人再被空中撞击的时候, 被施加一层GEEC, 一个GEEC负责把施法者的瞬时速度冻结, 防止窜的过快.

---
+++ Source/MMOARPG/Core/Game/Abilities/ExecutionCalculation/ExecutionCalculation_ZeroVelocity.cpp
@@ -0,0 +1,27 @@
+﻿#include "ExecutionCalculation_ZeroVelocity.h"
+#include "../../Character/Core/MMOARPGCharacterBase.h"
+#include "AbilitySystemComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+UExecutionCalculation_ZeroVelocity::UExecutionCalculation_ZeroVelocity()
+{
+
+}
+
+void UExecutionCalculation_ZeroVelocity::Execute_Implementation(
+	const FGameplayEffectCustomExecutionParameters& ExecutionParams,
+	OUT FGameplayEffectCustomExecutionOutput& OutExecutionOutput) const
+{
+	Super::Execute_Implementation(ExecutionParams, OutExecutionOutput);
+
+	UAbilitySystemComponent* TargetAC = ExecutionParams.GetTargetAbilitySystemComponent();
+	UAbilitySystemComponent* SourceAC = ExecutionParams.GetSourceAbilitySystemComponent();
+
+	if (TargetAC) {
+		if (AMMOARPGCharacterBase* InCharacter = Cast<AMMOARPGCharacterBase>(SourceAC->GetOwner())) {
+			if (InCharacter->GetCharacterMovement()) {
+				InCharacter->GetCharacterMovement()->StopMovementImmediately();
+			}
+		}
+	}
+}
