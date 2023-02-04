d750edcd9887e6cdbce87e97bdca384a9add975f
DESKTOP-6OVVB6Q\Askyr1m <akatokikwok@outlook.com>
2023-02-04 18:11:29 +0800

    [feat, phase1]:新增一个收到DamageGE后的Cue, 播放受击Cue特效并增加对应的GameplayTag.

---
+++ Source/MMOARPG/Core/Game/Abilities/Cue/GameplayCueNotify_Static_Hit.cpp
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+#include "GameplayCueNotify_Static_Hit.h"
+#include "NiagaraFunctionLibrary.h"
+#include "Kismet/GameplayStatics.h"
+
+void UGameplayCueNotify_Static_Hit::HandleGameplayCue(AActor* MyTarget, EGameplayCueEvent::Type EventType, const FGameplayCueParameters& Parameters)
+{
+	if (MyTarget && !MyTarget->IsPendingKill() && GetWorld()) {
+		switch (EventType) {
+			case EGameplayCueEvent::Executed:// 检查Cue是不是被执行了
+			{
+				if (HitEmitterTemplate) {
+					UGameplayStatics::SpawnEmitterAtLocation(
+						GetWorld(),
+						HitEmitterTemplate, MyTarget->GetActorTransform());
+				}
+				else if (HitNiagaraTemplate) {
+					UNiagaraFunctionLibrary::SpawnSystemAtLocation(
+						GetWorld(),
+						HitNiagaraTemplate, MyTarget->GetActorLocation());
+				}
+				break;
+			}
+		}
+	}
+
+	Super::HandleGameplayCue(MyTarget, EventType, Parameters);
+}
--- Source/MMOARPG/MMOARPG.Build.cs
+++ Source/MMOARPG/MMOARPG.Build.cs
@@ -29,7 +29,8 @@ public class MMOARPG : ModuleRules
             "SimpleHotUpdate",
             "SimpleNumericalDeductionRuntime",
             "SimplePopup",
-            "SimpleRuntimeGamePrinting"
+            "SimpleRuntimeGamePrinting",
+            "Niagara"
 
         });
 	}
