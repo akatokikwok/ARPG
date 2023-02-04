19103b92c02e3807537fb02277e13c7158d41eef
DESKTOP-6OVVB6Q\Askyr1m <akatokikwok@outlook.com>
2023-02-04 20:09:14 +0800

    [feat, phase3]:在冲刺伤害buff的GE蓝图里再增加一个GEEC效果:空中撞击敌人会授予敌人新的为6的受击ID.

---
+++ Source/MMOARPG/Core/Game/Abilities/ExecutionCalculation/ExecutionCalculation_ResetHitID.cpp
@@ -0,0 +1,48 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+#include "ExecutionCalculation_ResetHitID.h"
+#include "../../Character/Core/MMOARPGCharacterBase.h"
+#include "AbilitySystemComponent.h"
+
+UExecutionCalculation_ResetHitID::UExecutionCalculation_ResetHitID()
+	: HitIDByGround(INDEX_NONE)
+	, HitIDByAir(INDEX_NONE)
+	, bAuthorityPlayHit(true)
+{
+
+}
+
+void UExecutionCalculation_ResetHitID::Execute_Implementation(
+	const FGameplayEffectCustomExecutionParameters& ExecutionParams,
+	OUT FGameplayEffectCustomExecutionOutput& OutExecutionOutput) const
+{
+	Super::Execute_Implementation(ExecutionParams, OutExecutionOutput);
+
+	// 拿到自己的和敌人的ASC
+	UAbilitySystemComponent* TargetAC = ExecutionParams.GetTargetAbilitySystemComponent();
+	UAbilitySystemComponent* SourceAC = ExecutionParams.GetSourceAbilitySystemComponent();
+
+	if (TargetAC) {
+		if (AMMOARPGCharacterBase* InCharacter = Cast<AMMOARPGCharacterBase>(TargetAC->GetOwner())) {
+			if (!InCharacter->IsDie()) {
+				bool bPlay = false;
+				if (InCharacter->GetFightComponent()->IsAir()) {
+					if (HitIDByAir != INDEX_NONE) {
+						InCharacter->SetHitID(HitIDByAir);
+						bPlay = true;
+					}
+				}
+				else {
+					if (HitIDByGround != INDEX_NONE) {
+						InCharacter->SetHitID(HitIDByGround);
+						bPlay = true;
+					}
+				}
+
+				// 放受击技能
+				if (bAuthorityPlayHit && bPlay) {
+					InCharacter->PlayHit();
+				}
+			}
+		}
+	}
+}
