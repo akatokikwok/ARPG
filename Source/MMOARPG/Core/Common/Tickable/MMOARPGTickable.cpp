7c6529c308c0f5ca4ba95f7144ebaff09b696ab0
DESKTOP-6OVVB6Q\Askyr1m <akatokikwok@outlook.com>
2023-02-05 21:25:04 +0800

    [bugfix, arch]:改用专门的单例类(因为只有1个实例)来修复GameInstance里使用协程tick诱发的bug,多个客户端实例都在协程tick,导致时间过快

---
+++ Source/MMOARPG/Core/Common/Tickable/MMOARPGTickable.cpp
@@ -0,0 +1,33 @@
+﻿#include "MMOARPGTickable.h"
+#include "ThreadManage.h"
+
+FMMOARPGTickable* FMMOARPGTickable::MMOARPGTickableSinglePtr = nullptr;
+
+FMMOARPGTickable* FMMOARPGTickable::Get()
+{
+	if (!MMOARPGTickableSinglePtr) {
+		MMOARPGTickableSinglePtr = new FMMOARPGTickable();
+	}
+
+	return MMOARPGTickableSinglePtr;
+}
+
+void FMMOARPGTickable::Destroy()
+{
+	if (MMOARPGTickableSinglePtr) {
+		delete MMOARPGTickableSinglePtr;
+	}
+
+	MMOARPGTickableSinglePtr = nullptr;
+}
+
+// 本类的tick使用协程形式的tick
+void FMMOARPGTickable::Tick(float DeltaTime)
+{
+	GThread::Get()->Tick(DeltaTime);
+}
+
+TStatId FMMOARPGTickable::GetStatId() const
+{
+	return TStatId();
+}
---
+++ Source/MMOARPG/Core/Common/Tickable/MMOARPGTickable.h
@@ -0,0 +1,24 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Tickable.h"
+
+/**
+ * 主要为了游戏中需要被单独tick的 单例tick 如果在项目里面 多个客户端可能会同一帧tick该单例
+ */
+class FMMOARPGTickable :public FTickableGameObject
+{
+public:
+	static FMMOARPGTickable* Get();
+	static void Destroy();
+public:
+	// 本类的tick使用协程形式的tick
+	virtual void Tick(float DeltaTime);
+
+	virtual TStatId GetStatId() const;
+
+private:
+	static FMMOARPGTickable* MMOARPGTickableSinglePtr;
+};
