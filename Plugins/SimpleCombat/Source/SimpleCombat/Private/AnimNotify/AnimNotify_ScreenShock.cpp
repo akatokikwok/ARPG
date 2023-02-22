8dd08fe28374c16dd20beac8f37d855be998e4fc
DESKTOP-NONF38J\Administrator <akatokikwok@outlook.com>
2023-02-22 18:54:14 +0800

    [feat, phase2]:制作1个ComboCamShake并应用在蒙太奇的动画通知上; 增加已有的代码注释.

--- Plugins/SimpleCombat/Source/SimpleCombat/Private/AnimNotify/AnimNotify_ScreenShock.cpp
+++ Plugins/SimpleCombat/Source/SimpleCombat/Private/AnimNotify/AnimNotify_ScreenShock.cpp
@@ -8,9 +8,9 @@
 
 UAnimNotify_ScreenShock::UAnimNotify_ScreenShock(const FObjectInitializer& ObjectInitializer)
 	:Super(ObjectInitializer)
-	, bCustomShockClass(false)
-	, Scale(1.f)
-	, PlaySpace(ECameraShakePlaySpace::CameraLocal)
+	, bCustomShockClass(false)// 不勾选自定义模式
+	, Scale(1.f)// 振幅倍率
+	, PlaySpace(ECameraShakePlaySpace::CameraLocal)// 播放坐标空间
 	, UserPlaySpaceRot(FRotator::ZeroRotator)
 {
 	RootShakePattern = static_cast<UPerlinNoiseCameraShakePattern*>(ObjectInitializer.CreateDefaultSubobject(
@@ -33,17 +33,18 @@ void UAnimNotify_ScreenShock::Notify(USkeletalMeshComponent* MeshComp, UAnimSequ
 	Super::Notify(MeshComp, Animation, EventReference);
 
 	if (ACharacter* InCharacter = Cast<ACharacter>(MeshComp->GetOuter())) {
-		if (InCharacter->GetWorld() && !InCharacter->GetWorld()->IsNetMode(ENetMode::NM_DedicatedServer)) {
+		if (InCharacter->GetWorld() && !InCharacter->GetWorld()->IsNetMode(ENetMode::NM_DedicatedServer)) {// 仅在客户端
 			if (APlayerController* InPlayerController = Cast<APlayerController>(InCharacter->GetController())) {
-				if (InPlayerController->PlayerCameraManager) {
-					if (!bCustomShockClass) {
-						//保证里面有值
+				if (InPlayerController->PlayerCameraManager != nullptr) {// 拿到相机manager
+					if (!bCustomShockClass) {/* 未启用自定义震动模式 */
+						
+						// 确保设定了震荡根模式(例如)是正弦波振荡，柏林噪声，或FBX动画
 						if (!RootShakePattern) {
 							return;
 						}
-
-						if (AComboPlayerCameraManager* InCameraManager = Cast<AComboPlayerCameraManager>(InPlayerController->PlayerCameraManager)) {
-							if (UCameraShakeBase* InShakeBase = InCameraManager->PlayCameraShake(
+						// 
+						if (AComboPlayerCameraManager* ComboCamManager = Cast<AComboPlayerCameraManager>(InPlayerController->PlayerCameraManager)) {
+							if (UCameraShakeBase* InShakeBase = ComboCamManager->PlayCameraShake(
 								UComboCameraShake::StaticClass(),
 								FOnInitializeCameraShake::CreateUObject(this, &UAnimNotify_ScreenShock::InitializeCameraShake),
 								Scale,
@@ -53,9 +54,9 @@ void UAnimNotify_ScreenShock::Notify(USkeletalMeshComponent* MeshComp, UAnimSequ
 							}
 						}
 					}
-					else {
-						if (UCameraShakeBase* InShakeBase = InPlayerController->PlayerCameraManager->
-							StartCameraShake(ShakeClass, Scale, PlaySpace, UserPlaySpaceRot)) {
+					else {/* 启用了自定义震动模式. */
+						if (UCameraShakeBase* InShakeBase =
+							InPlayerController->PlayerCameraManager->StartCameraShake(ShakeClass, Scale, PlaySpace, UserPlaySpaceRot)) {// 这种情况则 播放普通通用的相机震动
 
 						}
 					}
--- Plugins/SimpleCombat/Source/SimpleCombat/Public/AnimNotify/AnimNotify_ScreenShock.h
+++ Plugins/SimpleCombat/Source/SimpleCombat/Public/AnimNotify/AnimNotify_ScreenShock.h
@@ -26,22 +26,27 @@ protected:
 	void InitializeCameraShake(UCameraShakeBase* InCameraShakeBase);
 
 protected:
+	// 自定义模式勾选
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Screen Shock")
 		bool bCustomShockClass;
 
-	/** The root pattern for this camera shake */
+	/** 相机震荡的根模式, (例如)是正弦波振荡，柏林噪声，或FBX动画 */
 	UPROPERTY(EditAnywhere, Instanced, Category = "Screen Shock", meta = (EditCondition = "!bCustomShockClass"))
 		TObjectPtr<UCameraShakePattern> RootShakePattern;
 
+	// 自定义模式勾选后会启用的 相机震动的tclass
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Screen Shock", meta = (EditCondition = "bCustomShockClass"))
 		TSubclassOf<UCameraShakeBase> ShakeClass;
 
+	// 自定义模式勾选后会启用的 震动振幅
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Screen Shock", meta = (EditCondition = "bCustomShockClass"))
 		float Scale;
 
+	// 自定义模式勾选后会启用的 震动应用在的3D模式
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Screen Shock", meta = (EditCondition = "bCustomShockClass"))
 		ECameraShakePlaySpace PlaySpace;
-
+	
+	// 自定义模式勾选后会启用的 用户自定义的相机震动旋转幅度
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Screen Shock", meta = (EditCondition = "bCustomShockClass"))
 		FRotator UserPlaySpaceRot;
 };
