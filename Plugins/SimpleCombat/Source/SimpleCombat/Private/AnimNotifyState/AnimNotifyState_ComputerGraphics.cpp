431a84a3b3872fed4369931a6b3f80ae29ea8f8b
DESKTOP-NONF38J\Administrator <akatokikwok@outlook.com>
2023-02-23 18:31:29 +0800

    [feat, phase2]: 添加1个用以在有效时间段内进行大招相机跳转的NotifyState.

---
+++ Plugins/SimpleCombat/Source/SimpleCombat/Private/AnimNotifyState/AnimNotifyState_ComputerGraphics.cpp
@@ -0,0 +1,98 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+#include "AnimNotifyState/AnimNotifyState_ComputerGraphics.h"
+#include "SimpleComboType.h"
+#include "GameFramework/Character.h"
+#include "Camera/CameraActor.h"
+#include "Camera/CameraComponent.h"
+
+#if WITH_EDITOR
+#include "AnimationEditorPreviewActor.h"
+#endif
+
+UAnimNotifyState_ComputerGraphics::UAnimNotifyState_ComputerGraphics()
+	: bControllerRotation(false)
+	, bCameraHoming(true)
+	, bPlayCameraAnim(false)
+{
+	FieldOfView = 90.0f;
+	AspectRatio = 1.777778f;
+	OrthoWidth = 512.0f;
+	OrthoNearClipPlane = 0.0f;
+	OrthoFarClipPlane = WORLD_MAX;
+	bConstrainAspectRatio = false;
+	bUseFieldOfViewForLOD = true;
+	PostProcessBlendWeight = 1.0f;
+	ProjectionMode = ECameraProjectionMode::Perspective;
+}
+
+void UAnimNotifyState_ComputerGraphics::NotifyBegin(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float TotalDuration, const FAnimNotifyEventReference& EventReference)
+{
+	Super::NotifyBegin(MeshComp, Animation, TotalDuration, EventReference);
+
+	if (AActor* InCharacter = Cast<AActor>(MeshComp->GetOuter())) {
+		// 仅在客户端 或者是编辑器开启调试的时候 生效
+		if (InCharacter->GetLocalRole() == ENetRole::ROLE_AutonomousProxy
+		#if WITH_EDITOR
+			|| Cast<AAnimationEditorPreviewActor>(MeshComp->GetOuter())
+		#endif
+			) {
+
+			// Spawn参数
+			FActorSpawnParameters SpawnParameters;
+			SpawnParameters.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+			// 相机位置 = 人坐标加一段偏移向量
+			FVector CamaraPos = InCharacter->GetActorLocation() + RelativePos;
+			// 若不想使用自己控制的controller去直接控制旋转, 则需要自己计算一套逻辑; 让相机看向人
+			FRotator NewRot = PreRotation;
+			if (!bControllerRotation) {
+				FVector ViewPos = CameraViewOffsetValue + InCharacter->GetActorLocation();
+				// 对应蓝图里的API : FindLookAtRotation; 用人视角位置减去相机位置
+				NewRot = FRotationMatrix::MakeFromX(ViewPos - CamaraPos).Rotator();
+			}
+
+			// 观察视角跳转至场景生成相机.
+			if (ACameraActor* InSpawnedCamera = InCharacter->GetWorld()->SpawnActor<ACameraActor>(ACameraActor::StaticClass(), CamaraPos, NewRot, SpawnParameters)) {
+				// 设定相机参数
+				{
+					InSpawnedCamera->SetLifeSpan(TotalDuration);// actor存续时长
+					InSpawnedCamera->GetCameraComponent()->SetFieldOfView(FieldOfView);// FOV
+					InSpawnedCamera->GetCameraComponent()->SetOrthoWidth(OrthoWidth);// 正交投影宽
+					InSpawnedCamera->GetCameraComponent()->SetOrthoNearClipPlane(OrthoNearClipPlane);// 近裁剪面
+					InSpawnedCamera->GetCameraComponent()->SetOrthoFarClipPlane(OrthoFarClipPlane);// 远裁剪面
+					InSpawnedCamera->GetCameraComponent()->SetAspectRatio(AspectRatio);// AspectRatio
+					InSpawnedCamera->GetCameraComponent()->SetConstraintAspectRatio(bConstrainAspectRatio);// 限制ratio开关
+					InSpawnedCamera->GetCameraComponent()->SetUseFieldOfViewForLOD(bUseFieldOfViewForLOD);// LOD开关
+					InSpawnedCamera->GetCameraComponent()->SetProjectionMode(ProjectionMode);// 透视投影
+					InSpawnedCamera->GetCameraComponent()->PostProcessSettings = MyPostProcessSettings;// 后处理设定集
+				}
+				// 跳转至场景内上文生成的预设相机
+				if (ACharacter* InMyCharacter = Cast<ACharacter>(InCharacter)) {
+					if (APlayerController* InController = Cast<APlayerController>(InMyCharacter->GetController())) {
+						InController->SetViewTargetWithBlend(InSpawnedCamera, bPlayCameraAnim ? TotalDuration : 0.f);
+					}
+				}
+			}
+		}
+	}
+}
+
+void UAnimNotifyState_ComputerGraphics::NotifyTick(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float FrameDeltaTime, const FAnimNotifyEventReference& EventReference)
+{
+	Super::NotifyTick(MeshComp, Animation, FrameDeltaTime, EventReference);
+}
+
+void UAnimNotifyState_ComputerGraphics::NotifyEnd(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference)
+{
+	Super::NotifyEnd(MeshComp, Animation, EventReference);
+
+	// 启用相机复位,复位至人身上,再次观察人
+	if (bCameraHoming) {
+		if (ACharacter* InCharacter = Cast<ACharacter>(MeshComp->GetOuter())) {
+			if (InCharacter->GetLocalRole() == ENetRole::ROLE_AutonomousProxy) {
+				if (APlayerController* InController = Cast<APlayerController>(InCharacter->GetController())) {
+					InController->SetViewTargetWithBlend(InCharacter, 0.f);
+				}
+			}
+		}
+	}
+}
---
+++ Plugins/SimpleCombat/Source/SimpleCombat/Public/AnimNotifyState/AnimNotifyState_ComputerGraphics.h
@@ -0,0 +1,88 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotifyState.h"
+#include "AnimNotifyState_ComputerGraphics.generated.h"
+
+/**
+ *
+ */
+UCLASS(meta = (DisplayName = "ComputerGraphics"))
+class SIMPLECOMBAT_API UAnimNotifyState_ComputerGraphics : public UAnimNotifyState
+{
+	GENERATED_BODY()
+
+public:
+	UAnimNotifyState_ComputerGraphics();
+	virtual void NotifyBegin(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float TotalDuration, const FAnimNotifyEventReference& EventReference);
+	virtual void NotifyTick(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float FrameDeltaTime, const FAnimNotifyEventReference& EventReference);
+	virtual void NotifyEnd(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference);
+
+public:
+	// 预设距离人的偏移(偏移多远到场景相机)
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "ComputerGraphics")
+		FVector CameraViewOffsetValue;
+
+	// 用这段偏移值偏移到相机
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "ComputerGraphics")
+		FVector RelativePos;
+
+	// 是否启用控制器旋转权
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "ComputerGraphics")
+		bool bControllerRotation;
+
+	// 近裁剪面
+	UPROPERTY(Interp, EditAnywhere, BlueprintReadWrite, Category = CameraSettings)
+		float OrthoNearClipPlane;
+
+	// 预设偏移旋转分量
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "ComputerGraphics", meta = (EditCondition = "bControllerRotation"))
+		FRotator PreRotation;
+
+	// 是否启用相机复位,复位至人身上
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "ComputerGraphics")
+		bool bCameraHoming;
+
+
+	// 是否开启相机动画平滑过渡至跳转场景相机
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "ComputerGraphics")
+		bool bPlayCameraAnim;
+
+	// FOV
+	UPROPERTY(Interp, EditAnywhere, BlueprintReadWrite, Category = CameraSettings, meta = (UIMin = "5.0", UIMax = "170", ClampMin = "0.001", ClampMax = "360.0", Units = deg))
+		float FieldOfView;
+
+	// 正交投影宽
+	UPROPERTY(Interp, EditAnywhere, BlueprintReadWrite, Category = CameraSettings)
+		float OrthoWidth;
+
+	// 远裁剪面
+	UPROPERTY(Interp, EditAnywhere, BlueprintReadWrite, Category = CameraSettings)
+		float OrthoFarClipPlane;
+
+	// AspectRatio
+	UPROPERTY(Interp, EditAnywhere, BlueprintReadWrite, Category = CameraSettings, meta = (ClampMin = "0.1", ClampMax = "100.0", EditCondition = "bConstrainAspectRatio"))
+		float AspectRatio;
+
+	// 限制ratio开关
+	UPROPERTY(Interp, EditAnywhere, BlueprintReadWrite, Category = CameraOptions)
+		uint8 bConstrainAspectRatio : 1;
+
+	// LOD开关
+	UPROPERTY(Interp, EditAnywhere, AdvancedDisplay, BlueprintReadWrite, Category = CameraOptions)
+		uint8 bUseFieldOfViewForLOD : 1;
+
+	// 投影成像模式
+	UPROPERTY(Interp, EditAnywhere, BlueprintReadWrite, Category = CameraSettings)
+		TEnumAsByte<ECameraProjectionMode::Type> ProjectionMode;
+
+	/** Indicates if PostProcessSettings should be used when using this Camera to view through. */
+	UPROPERTY(Interp, EditAnywhere, BlueprintReadWrite, Category = PostProcess, meta = (UIMin = "0.0", UIMax = "1.0"))
+		float PostProcessBlendWeight;
+
+	// 后处理设定集
+	UPROPERTY(Interp, BlueprintReadWrite, Category = PostProcess, meta = (ShowOnlyInnerProperties))
+		struct FPostProcessSettings MyPostProcessSettings;
+};
