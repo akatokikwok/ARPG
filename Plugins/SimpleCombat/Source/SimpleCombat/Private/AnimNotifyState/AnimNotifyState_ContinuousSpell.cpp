57b156b7a6214794decf3694da826127002efdae
DESKTOP-NONF38J\Administrator <akatokikwok@outlook.com>
2023-02-28 11:58:37 +0800

    [arch, feat, phase3]: 制作notifystate用来操控持续施法黑盒,以达到耗蓝施法的蒙太奇效果.

---
+++ Plugins/SimpleCombat/Source/SimpleCombat/Private/AnimNotifyState/AnimNotifyState_ContinuousSpell.cpp
@@ -0,0 +1,58 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "AnimNotifyState/AnimNotifyState_ContinuousSpell.h"
+#include "SimpleComboType.h"
+#include "CombatInterface/SimpleCombatInterface.h"
+#include "GameFramework/Character.h"
+
+void UAnimNotifyState_ContinuousSpell::NotifyBegin(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float TotalDuration, const FAnimNotifyEventReference& EventReference)
+{
+	Super::NotifyBegin(MeshComp, Animation, TotalDuration, EventReference);
+
+	if (MeshComp->GetOuter() && MeshComp->GetOuter()->GetWorld()) {
+		if (ISimpleComboInterface* InCharacter = Cast<ISimpleComboInterface>(MeshComp->GetOuter())) {
+			if (FContinuousReleaseSpell* InSpell = InCharacter->GetContinuousReleaseSpell()) {
+				if (InSpell->ContinuousReleaseSpellIndex == 0 ||
+					InSpell->ContinuousReleaseSpellIndex == INDEX_NONE) {
+					// 进入下一个循环
+					InSpell->ContinuousReleaseSpellIndex = 1;
+				}
+			}
+		}
+	}
+}
+
+void UAnimNotifyState_ContinuousSpell::NotifyTick(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float FrameDeltaTime, const FAnimNotifyEventReference& EventReference)
+{
+	Super::NotifyTick(MeshComp, Animation, FrameDeltaTime, EventReference);
+}
+
+void UAnimNotifyState_ContinuousSpell::NotifyEnd(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference)
+{
+	Super::NotifyEnd(MeshComp, Animation, EventReference);
+
+	if (MeshComp->GetOuter() && MeshComp->GetOuter()->GetWorld()) {
+		if (ISimpleComboInterface* InCharacterInterface = Cast<ISimpleComboInterface>(MeshComp->GetOuter())) {
+			if (FContinuousReleaseSpell* InSpell = InCharacterInterface->GetContinuousReleaseSpell()) {
+				if (InSpell->AnimMontage) {
+					FName SectionName = *FString::FromInt(InSpell->ContinuousReleaseSpellIndex);
+					if (ACharacter* InCharacter = Cast<ACharacter>(MeshComp->GetOuter())) {
+						if (InSpell->ContinuousReleaseSpellIndex == 2) {
+							InCharacter->PlayAnimMontage(
+								InSpell->AnimMontage, 1.f,
+								SectionName);
+						}
+						else {
+							if (InCharacter->GetMesh()) {
+								if (UAnimInstance* InAnimInstance = InCharacter->GetMesh()->GetAnimInstance()) {
+									InAnimInstance->Montage_JumpToSection(SectionName);
+									InAnimInstance->Montage_SetNextSection(SectionName, SectionName);
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+}
